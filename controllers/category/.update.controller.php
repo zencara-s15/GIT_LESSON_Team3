94c7dcd201f76e5b93cfb7040652c2fe9497b8c4
Siem-Choub <siem.choub@student.passerellesnumeriques.org>
2024-02-08 11:11:35 +0700

    [imp] this is the GLT3-8

---
+++ controllers/category/.update.controller.php
@@ -0,0 +1,15 @@
+<?php
+require '../../database/database.php';
+$reach = 10;
+$hor = "I love reach so much";
+$siem = 'Hi';
+$bro = "love you";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    if (!empty($_POST['title']) and !empty($_POST['description'])) {
+        require_once('../../models/post.model.php');
+        updatePost($_POST['title'], $_POST['description'], $_POST['id']);
+
+        header('location: /post');
+    }
+}
